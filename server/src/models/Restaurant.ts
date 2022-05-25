39e46adbf314f55686e23856aa7d34f63515f61a
marcoslima12 <marcosantonio-10@live.com>
2022-05-25 08:31:35 -0300

    feature(desafio-crud-restaurant): Criei o modelo de restaurante, os contradorese defini as rotas

---
+++ server/src/models/Restaurant.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Restaurant{
+    
+    @PrimaryGeneratedColumn()
+    id: number 
+
+    @Column()
+    name: string 
+
+    @Column()
+    adress: string
+
+    @Column()
+    foodType: string
+}
